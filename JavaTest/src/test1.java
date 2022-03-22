2f29a2777fe32bbf2b65b604cd072bf3ffcb8b98
ARUN <arunlakshmanmech@gmail.com>
2022-03-22 19:51:16 +0530

    first commit

---
+++ JavaTest/src/test1.java
@@ -0,0 +1,19 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   char count='F';
+		   for(char i=count;i>='A';i--)
+		{
+		for(char j='A';j<=i;j++)
+		{
+		System.out.print(j);
+		}
+		System.out.println(" ");
+		}
+			
+	}
+}
+	
+
